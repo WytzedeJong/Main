9518b8d85cc3466431e9a04b3e072f93e5beaf09
WytzedeJong <S1208090@student.windesheim.nl>
2026-04-21 11:55:04 +0200

    1 minute dungeon aangepast, space game toegevoegd

--- ui/Games_menu.py
+++ ui/Games_menu.py
@@ -3,9 +3,9 @@ import datetime
 from core.scene import Scene
 from settings import base_surface, screen, BASE_WIDTH, BASE_HEIGHT
 from config import styles
-from games.adventure.game import PuzzleGame
+from games.puzzle.game import PuzzleGame
 from games.Pengu_Slider.game import AdventureGame
-from games.racer.game import RacerGame
+from games.Space.game import SpaceGame
 from games.dungeon.game import DungeonGame
 # from games.pinguin_slider.game import PinguinSlider
 from games.monkey_stacker.game import MonkeyStacker
@@ -22,7 +22,7 @@ class Game_Menu(Scene):
             ("Puzzle", PuzzleGame),
             ("Dungeon", DungeonGame),
             ("Pengu Slider", AdventureGame),
-            #("Hello", RacerGame),
+            ("Space", SpaceGame),
             ("Monkey", MonkeyStacker),
             #("Pingiun", PinguinSlider)
         ]
